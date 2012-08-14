047dcd4cccd77b3bfc5d586cf9bd3e38380512ad
Jose Guilherme Vanz <guilherme.sft@gmail.com>
2012-08-14 19:36:44 -0300

    Adding a new file will contain a lot of functions manapulatin lists

---
+++ datastruct/listmegazorde.c
@@ -0,0 +1,83 @@
+/*
+ *The source contais a lot os functions manipulating list
+ *
+ * */
+#include<stdio.h>
+
+struct node
+{
+	int value;
+	struct node *nextNode;
+};
+
+typedef struct node ListNode;
+typedef ListNode *nodePointer;
+
+
+int concateList(nodePointer,nodePointer);
+void printList(nodePointer);
+int isEmpty(nodePointer);
+
+int main(void)
+{
+	nodePointer startNode = NULL;
+
+	startNode = malloc(sizeof(ListNode));	
+	if(startNode != NULL){
+		startNode->value = 1;
+		nodePointer scdNode = malloc(sizeof(ListNode));
+		scdNode->value = 2;
+		scdNode->nextNode = NULL;
+		startNode->nextNode = scdNode;
+	}
+	printList(startNode);
+	return 0;
+}
+
+/**
+ *This function contace the second list on the first one.
+ *
+ * */
+int concateList(nodePointer mainList, nodePointer secondList)
+{
+	nodePointer currentNode = mainList;
+	nodePointer tempNode;
+	
+	if(currentNode != NULL){
+		while(currentNode->nextNode != NULL){
+			currentNode = currentNode->nextNode;
+		}
+		if(secondList != NULL){
+			currentNode->nextNode = secondList;
+		}
+	}
+
+}
+
+/*
+ *The function verify if a list is empty
+ * */
+int isEmpty(nodePointer list)
+{
+	return list == NULL;
+}
+
+/*
+ *Print all elements of the list
+ * */
+void printList(nodePointer list)
+{
+	nodePointer currentNode = list;
+	if(!isEmpty(list)){
+		printf("The list's values are: \n");	
+		while(currentNode != NULL){
+			int value = currentNode->value;
+			printf("%d -> ", value);
+			currentNode = currentNode->nextNode;
+		}
+		printf(" NULL");
+	}else{
+		printf("The list is empty, idiot!");
+	}
+}
+
