c872a144d870bbc60244210837f08154e58f3de0
José Guilherme Vanz <guilherme.sft@gmail.com>
2012-10-13 16:53:40 -0300

    Termino de implementação da leitura do arquivo de entrada

--- FURB/grafos/dijkstra.c
+++ FURB/grafos/dijkstra.c
@@ -1,35 +1,50 @@
 /*
- * Implementação da questão 1 do trabalho 
+ * Implementação da questão 1 do trabalho.
+ * O prgrama espera um arquivo chamado entrada.txt para ser executado. Esse arquivo deve conter uma matrix como 
+ * espeficiado no enunciado do trabalho
  * Aluno: José Guilherme Vanz
  * */
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-void leArquivoEntrada();
 void aplicaDijkstra();
 
 int main()
 {
-	leArquivoEntrada();
 	aplicaDijkstra();
 }
 
-/* Le arquivo de entrada e cria o grafo*/
-void leArquivoEntrada()
+/* Le arquivo de entrada e aplica dijkstra no grafo*/
+void aplicaDijkstra()
 {
 	FILE *arquivo = fopen("entrada.txt","r");
 	if(arquivo){ /*Verifica se arquivo existe*/
 		printf("Arquivo encontrado\n");
 		int numVertices = -1;
-		char[][] matrixAdj;
+		char origem,destino,fora;
 		if(fscanf(arquivo,"%d", &numVertices)){
+			int matrixAdj[numVertices][numVertices];
+			memset(matrixAdj,-1,sizeof(matrixAdj));
 			printf("Número de vertices %d\n", numVertices);
+			fscanf(arquivo,"%c",&origem); /*Pula quebra linha*/
+			fscanf(arquivo,"%c",&origem);
+			fscanf(arquivo,"%c",&destino); /*Pula espaço em branco*/
+			fscanf(arquivo,"%c",&destino);
+			fscanf(arquivo,"%c",&fora);
+			printf("Caminho entre %c e %c\n",origem,destino);
+			int linha = 0,coluna = 0;
 			while(!feof(arquivo)){ /* Lê arquivo até o final*/
 				char c;
-				
-				}else{
-					printf("Erro na leitura do arquivo\n");
+				fscanf(arquivo,"%c",&c);
+				if(c == '\n'){ /*Final de linha, muda o vertice*/
+					linha++;
+					coluna = 0;
+				}else if(isdigit(c)){ /*Valor de aresta*/
+					matrixAdj[linha][coluna] = atoi(&c);
+				}else if(c == ' '){ /*Passo de coluna*/
+					coluna++;
 				}
 			}
 		}else{
@@ -39,10 +54,7 @@ void leArquivoEntrada()
 		printf("Arquivo de entrada.txt não encontrado. Verifique!\n");
 	}	
 	
-}
+	/*------------ Termino da leitura. Aplica dijkstra no grafo --------------*/
 
-/* Aplica dijkstra verifica o menor caminho entre os pontos*/
-void aplicaDijkstra()
-{
 
 }
