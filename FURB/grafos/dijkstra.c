41342151b30c4a1051e581e9e133b22f26171914
Jose Guilherme Vanz <guilherme.sft@gmail.com>
2012-10-13 14:25:23 -0300

    Inicio de implementação do trabalho de grafos

--- FURB/grafos/dijkstra.c
+++ FURB/grafos/dijkstra.c
@@ -1,8 +1,48 @@
-#include"grafos.h"
+/*
+ * Implementação da questão 1 do trabalho 
+ * Aluno: José Guilherme Vanz
+ * */
 
+#include<stdio.h>
+#include<stdlib.h>
+
+void leArquivoEntrada();
+void aplicaDijkstra();
 
 int main()
 {
-/*TODO*/
+	leArquivoEntrada();
+	aplicaDijkstra();
 }
 
+/* Le arquivo de entrada e cria o grafo*/
+void leArquivoEntrada()
+{
+	FILE *arquivo = fopen("entrada.txt","r");
+	if(arquivo){ /*Verifica se arquivo existe*/
+		printf("Arquivo encontrado\n");
+		int numVertices = -1;
+		char[][] matrixAdj;
+		if(fscanf(arquivo,"%d", &numVertices)){
+			printf("Número de vertices %d\n", numVertices);
+			while(!feof(arquivo)){ /* Lê arquivo até o final*/
+				char c;
+				
+				}else{
+					printf("Erro na leitura do arquivo\n");
+				}
+			}
+		}else{
+			printf("Erro na leitura do arquivo. Não foi possível ler o número de vértices na primeira linha.\n");
+		}		
+	}else{
+		printf("Arquivo de entrada.txt não encontrado. Verifique!\n");
+	}	
+	
+}
+
+/* Aplica dijkstra verifica o menor caminho entre os pontos*/
+void aplicaDijkstra()
+{
+
+}
