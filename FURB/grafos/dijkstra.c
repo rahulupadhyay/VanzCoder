585f9f175633f1011f2c31cfd3fc5b95dd63b3a4
José Guilherme Vanz <guilherme.sft@gmail.com>
2012-10-14 03:22:20 -0300

    Ajuste final no algoritmo de Dijkstra

--- FURB/grafos/dijkstra.c
+++ FURB/grafos/dijkstra.c
@@ -8,6 +8,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<limits.h>
 
 void aplicaDijkstra();
 
@@ -48,39 +49,37 @@ void aplicaDijkstra()
 				}
 			}
 			/*------------ Termino da leitura. Aplica dijkstra no grafo --------------*/
-
 			int matrixDijkstra[numVertices][4];
 			int pivo,i;
-			memset(matrixDijkstra,-1,sizeof(matrixAdj)); /*Limpa tabela*/
+			memset(matrixDijkstra,-1,sizeof(matrixDijkstra)); /*Limpa tabela*/
 			for(i = 0; i < numVertices;i++){ /*Inicializa*/
 				matrixDijkstra[i][2] = 1;
 				matrixDijkstra[i][3] = 0;
 			}
 			matrixDijkstra[origem-65][0] = 0;
-			
-			int x,y = 0;
-			for(x = 0; x < numVertices; x++){
-				for(y = 0; y < 4; y++){	
-					printf("%3d ", matrixDijkstra[x][y]);
-				}
-				printf("\n");
-			}
-	
+			pivo = -1;	
 			while(1){
 				int index,processar = 0;
 				for(index = 0; index < numVertices; index++){
 					if(matrixDijkstra[index][2])
-					processar = 1;
+						processar = 1;
+						break;
 				}
 				if(!processar)
 					break;
 				/*----------------Pega minimo-------------------------*/
-				int min = -1;
+				int min = INT_MAX;
 				for(index = 0; index < numVertices; index++){
 					if(matrixDijkstra[index][2]){
-						if((min == -1) | (matrixDijkstra[index][0] < min)){
-							pivo = index;
-							min = matrixDijkstra[index][0];
+						if(pivo == -1){
+							if(matrixDijkstra[index][0] == 0){
+								pivo = index;
+							}
+						}else{
+							if(matrixDijkstra[index][0] > 0 && matrixDijkstra[index][0] < min ){
+								pivo = index;
+								min = matrixDijkstra[index][0];
+							}
 						}
 					}
 				}
@@ -88,34 +87,16 @@ void aplicaDijkstra()
 				matrixDijkstra[pivo][3] = 1; /*Processado*/
 				matrixDijkstra[pivo][2] = 0;
 				/*----- Percorre adjacentes -----*/
-				printf("PIVO = %c\n",(pivo + 65));
 				int adj;
 				for(adj = 0; adj < numVertices; adj++ ){
 					if(matrixAdj[pivo][adj] >= 0 && pivo != adj ){
-						//printf("Custo atual = %d ----- Custo calculado = %d\n", matrixDijkstra[adj][0], (matrixDijkstra[pivo][0] + matrixAdj[pivo][adj]));
 						if((matrixDijkstra[adj][0] == -1) || ( matrixDijkstra[adj][0] > (matrixDijkstra[pivo][0] + matrixAdj[pivo][adj]))){
 							matrixDijkstra[adj][0] = matrixDijkstra[pivo][0] + matrixAdj[pivo][adj];
 							matrixDijkstra[adj][1] = pivo;
-							printf("Novo custo p/ %d = %d\n", adj,(matrixDijkstra[pivo][0] + matrixAdj[pivo][adj]));
-							printf("Novo pai p/ %d = %d\n",adj, pivo);
 						}
 					}
 				}
 			}
-			printf("\n");
-			for(x = 0; x < numVertices; x++){
-				printf("%c -> ", (65 + x));
-				for(y = 0; y < 4; y++){	
-					if(y == 1){
-						printf("%c", (matrixDijkstra[x][y]+65));
-					}else{
-						printf("%3d ", matrixDijkstra[x][y]);
-					}
-				}
-				printf("\n");
-			}
-	
-
 		}else{
 			printf("Erro na leitura do arquivo. Não foi possível ler o número de vértices na primeira linha.\n");
 		}		
