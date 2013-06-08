82b0af6493cc7fe5123bc1c2fc97c6ef558e1de3
guilhermesft <guilherme.sft@gmail.com>
2013-06-08 09:53:55 -0300

    Adicionado obj e formantando fonte
    Esse commit adiciona os .obj utilizados nos curso de OpenGL e
    formata o código usando tabs

--- opengl/CursoCG.cpp
+++ opengl/CursoCG.cpp
@@ -23,22 +23,22 @@ void CreateBatch();
 
 // Classes auxiliares do GLTools
 GLShaderManager		shaderManager;     //The shader manager of GLTools
-GLFrame				cameraFrame;       //Frame of reference for the camera
-GLFrustum			viewFrustum;       //View frustum
+GLFrame			cameraFrame;       //Frame of reference for the camera
+GLFrustum		viewFrustum;       //View frustum
 GLMatrixStack		modelViewMatrix;   //Modelview Matrix
 GLMatrixStack		projectionMatrix;  //Projection Matrix
 GLGeometryTransform transformPipeline; //Geometry Transform Pipeline
 
 // Parametros da camera
-M3DVector3f			gCameraPosition =   {0.0f, 0.5f, 5.0f};    //Camera position
-M3DVector3f			gCameraLookAt =     {0.0f, 0.0f, 0.0f};     //Reference point to look at
-M3DVector3f			gCameraUp =         {0.0f, 1.0f, 0.0f};	    //Camera up direction
-GLfloat				gCameraYaw =        0.0f;					//Carmera Yaw
+M3DVector3f		gCameraPosition =   {0.0f, 0.5f, 5.0f};    //Camera position
+M3DVector3f		gCameraLookAt =     {0.0f, 0.0f, 0.0f};     //Reference point to look at
+M3DVector3f		gCameraUp =         {0.0f, 1.0f, 0.0f};	    //Camera up direction
+GLfloat			gCameraYaw =        0.0f;					//Carmera Yaw
 
 
 // Batchs
-GLBatch				axisBatch;			// Batch de geometria dos eixos
-GLBatch				linesBatch;			// Batch de geometria das linhas
+GLBatch			axisBatch;			// Batch de geometria dos eixos
+GLBatch			linesBatch;			// Batch de geometria das linhas
 GLTriangleBatch		sphereBatch;        // Batch de geometria da esfera
 
 // Variaveis auxiliares
@@ -84,19 +84,19 @@ void SpecialKeys(int key, int x, int y) {
 /* GameRender */
 
 void GameRender(void) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); // Limpa os buffers e deixa com a cor definida
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); // Limpa os buffers e deixa com a cor definida
     
-    // Desenha a cena
-    shaderManager.UseStockShader(GLT_SHADER_SHADED, transformPipeline.GetModelViewProjectionMatrix());
-    linesBatch.Draw();
+    	// Desenha a cena
+    	shaderManager.UseStockShader(GLT_SHADER_SHADED, transformPipeline.GetModelViewProjectionMatrix());
+    	linesBatch.Draw();
 	axisBatch.Draw();
     
-    // Translada e desenha a esfera
-    modelViewMatrix.Translate(10, 2, 10);
-    shaderManager.UseStockShader(GLT_SHADER_SHADED, transformPipeline.GetModelViewProjectionMatrix());
-    sphereBatch.Draw();
+    	// Translada e desenha a esfera
+    	modelViewMatrix.Translate(10, 2, 10);
+    	shaderManager.UseStockShader(GLT_SHADER_SHADED, transformPipeline.GetModelViewProjectionMatrix());
+    	sphereBatch.Draw();
 
-    glutSwapBuffers();
+    	glutSwapBuffers();
 }
 
 
@@ -110,11 +110,11 @@ void ChangeSize(int w, int h) {
 	windowWidth = w;
 	windowHeight = h;
 
-    viewFrustum.SetPerspective (45.0f, float(windowWidth)/float(windowHeight), 0.01f, 5000.f);
+    	viewFrustum.SetPerspective (45.0f, float(windowWidth)/float(windowHeight), 0.01f, 5000.f);
 }
 
 void CameraUpdate(void) {
-    projectionMatrix.LoadIdentity();
+    	projectionMatrix.LoadIdentity();
 	projectionMatrix.LoadMatrix (viewFrustum.GetProjectionMatrix());
     
 	M3DVector3f forwardVector = {0.0f, 0.0f, 0.0f};
@@ -147,18 +147,18 @@ void GameLogic(void) {
 /* MainCycle */
 
 void MainCycle(void){
-    clock_t nowClock = clock();
-    // Calcula quanto tempo levou desde a ultima chamada
-    // (TICKS_AGORA - ULTIMO_TICKS) / TICKS_POR_SEGUNDO * 100
-    float lastTimeUpdate = (((float)nowClock - (float)lastClock) / CLOCKS_PER_SEC * 100);
+    	clock_t nowClock = clock();
+    	// Calcula quanto tempo levou desde a ultima chamada
+    	// (TICKS_AGORA - ULTIMO_TICKS) / TICKS_POR_SEGUNDO * 100
+    	float lastTimeUpdate = (((float)nowClock - (float)lastClock) / CLOCKS_PER_SEC * 100);
 
-    // Verifica se deve atualizar a lógica
-    if(lastTimeUpdate > updateFrequency){
-        GameLogic();
-        lastClock = nowClock;
-    }
+    	// Verifica se deve atualizar a lógica
+    	if(lastTimeUpdate > updateFrequency){
+        	GameLogic();
+        	lastClock = nowClock;
+   	 }
 
-    CameraUpdate();
+    	CameraUpdate();
 	GameRender();
 }
 
@@ -170,10 +170,10 @@ void Init() {
 	glEnable(GL_DEPTH_TEST); // Habilita o buffer de teste de profundidade
 	glEnable(GL_CULL_FACE); // Habilita o culling de faces, por questoes de performance, desenhando apenas as faces da frente
     
-    // Inicializa o gerenciador de shaders
-    shaderManager.InitializeStockShaders();
+    	// Inicializa o gerenciador de shaders
+    	shaderManager.InitializeStockShaders();
     
-    CreateBatch();
+    	CreateBatch();
 }
 
 /* Main */
@@ -185,13 +185,13 @@ int main(int argc, char* argv[]){
 	glutInitWindowSize(windowWidth, windowHeight);
 	glutCreateWindow("CursoCG");
 
-    glutReshapeFunc(ChangeSize);
-    glutDisplayFunc(MainCycle);
+	glutReshapeFunc(ChangeSize);
+	glutDisplayFunc(MainCycle);
 	glutIdleFunc(MainCycle);
 	glutSpecialFunc(SpecialKeys);
 	glutKeyboardFunc(KeyboardFunc);
     
-    GLenum err = glewInit();
+   	GLenum err = glewInit();
 
 	if (GLEW_OK != err) {
 		fprintf(stderr, "GLEW Error: %s\n", glewGetErrorString(err));
@@ -209,40 +209,40 @@ int main(int argc, char* argv[]){
 /* CreateBatch */
 
 void CreateBatch(){
-    // Cria uma esfera
-    gltMakeSphere(sphereBatch, 1.0f, 26, 13);
+    	// Cria uma esfera
+	gltMakeSphere(sphereBatch, 1.0f, 26, 13);
     
-    // Cria os eixos
-    /* Initialise the axis batch with the vertices and color */
+    	// Cria os eixos
+    	/* Initialise the axis batch with the vertices and color */
 	axisBatch.Begin(GL_LINES, 6);
     
-    // x Axis
+	// x Axis
 	axisBatch.Color4f(1.0f, 0.0f, 0.0f, 1.0f);
 	axisBatch.Vertex3f(0.0f,0.0f,0.0f);
-    axisBatch.Color4f(1.0f, 0.0f, 0.0f, 1.0f);
+    	axisBatch.Color4f(1.0f, 0.0f, 0.0f, 1.0f);
 	axisBatch.Vertex3f(1.0f,0.0f,0.0f);
     
 	// y Axis
-    axisBatch.Color4f(0.0f, 1.0f, 0.0f, 1.0f);
+    	axisBatch.Color4f(0.0f, 1.0f, 0.0f, 1.0f);
 	axisBatch.Vertex3f(0.0f,0.0f,0.0f);
-    axisBatch.Color4f(0.0f, 1.0f, 0.0f, 1.0f);
+    	axisBatch.Color4f(0.0f, 1.0f, 0.0f, 1.0f);
 	axisBatch.Vertex3f(0.0f,1.0f,0.0f);
     
 	// z Axis
-    axisBatch.Color4f(0.0f, 0.0f, 1.0f, 1.0f);
+    	axisBatch.Color4f(0.0f, 0.0f, 1.0f, 1.0f);
 	axisBatch.Vertex3f(0.0f,0.0f,0.0f);
-    axisBatch.Color4f(0.0f, 0.0f, 1.0f, 1.0f);
+    	axisBatch.Color4f(0.0f, 0.0f, 1.0f, 1.0f);
 	axisBatch.Vertex3f(0.0f,0.0f,1.0f);
 	axisBatch.End();
 
-    // Cria as linhas
+    	// Cria as linhas
 	linesBatch.Begin(GL_LINES, 84);
 	linesBatch.Color4f(0.5,0.5,0.5,1.0f);
 	for (int i=-10;i<=10;i++) {
-        linesBatch.Vertex3f(100*i,-1,-1000);
-        linesBatch.Vertex3f(100*i,-1,1000);
-        linesBatch.Vertex3f(-1000,-1,100*i);
-        linesBatch.Vertex3f(1000,-1,100*i);
+        	linesBatch.Vertex3f(100*i,-1,-1000);
+	        linesBatch.Vertex3f(100*i,-1,1000);
+        	linesBatch.Vertex3f(-1000,-1,100*i);
+	        linesBatch.Vertex3f(1000,-1,100*i);
 	}
 	linesBatch.End();
 }
