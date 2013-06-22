9678c5972d158b52fd285d1c12a07f264a3808e2
José Guilherme Vanz <guilherme.sft@gmail.com>
2013-06-22 12:24:12 -0300

    Commit inicial do MyARVideo

--- opengl/ar/MyARVideo/src/com/example/myarvideo/MyARVideo.java
+++ opengl/ar/MyARVideo/src/com/example/myarvideo/MyARVideo.java
@@ -1,59 +1,22 @@
 package com.example.myarvideo;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
 
 public class MyARVideo extends Activity {
-
-	private AppStatus appStatus = AppStatus.NOT_INITIALIZED;
-
-	/*----------------Native methods-------------------*/
-
-	public native void initNativeApp();
-	
-	/*-------------------------------------------------*/
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		updateApp(appStatus.INICIALIZATION);
-
+		setContentView(R.layout.activity_my_arvideo);
 	}
 
 	@Override
-	protected void onPostCreate(Bundle savedInstanceState) {
-		super.onPostCreate(savedInstanceState);
-	}
-
-	public void updateApp(AppStatus status) {
-
-		switch (status) {
-			case INICIALIZATION:
-				initApp();
-				break;
-			case NATIVE_INICIALIZATION:
-				initNative();
-				break;
-			case RUNNING:
-				break;
-			case STOP:
-				break;
-		}
-
-	}
-
-	/*
-	 * Inicializa a parte que tem relacionamento com o QCAR
-	 */
-	private void initNative() {
-		// TODO Auto-generated method stub
-	}
-
-	/*
-	 * Inicializa a parte que não tem relação ao QCAR
-	 */
-	private void initApp() {
-		// TODO Auto-generated method stub
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.my_arvideo, menu);
+		return true;
 	}
 
 }
