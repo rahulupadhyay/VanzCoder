b777a501d70979aed26d03853e6c5adf236428a0
guilhermesft <guilherme.sft@gmail.com>
2013-07-08 23:38:49 -0300

    Continuação da leitura obj

--- opengl/space/WaveFrontUtil.cpp
+++ opengl/space/WaveFrontUtil.cpp
@@ -10,7 +10,38 @@ void WaveFrontUtil::ReadObjFile(const char* path)
 	if (myfile.is_open()){
 		while ( myfile.good() ){
 			getline (myfile,line);
+			int lineType = -1;
+			if(line[0] == 'v' & line[1] == ' '){
+				std::cout << "LINHA COM VERTICE" << std::endl;
+				lineType = 0;
+			}else if(line[0] == 'v' & line[1] == 't'){
+				std::cout << "COORDENADA DE TEXTURA" << std::endl;
+				lineType = 1;
+			}else if(line[0] == 'v' & line[1] == 'n'){
+				std::cout << "NORMALS" << std::endl;
+				lineType = 2;
+			}else if(line[0] == 'f' & line[1] == ' '){
+				std::cout << "FACE" << std::endl;
+				lineType = 3;
+			}
+			line.erase(0,2);
+			if(line[0] == ' '){
+				line.erase(0,1);
+			}
+			if(line[line.length()-1] == ' '){
+				line.erase(line.length() - 1, 1);
+			}
 			std::cout << line << std::endl;
+			if(lineType == 0){ // vertice
+
+			}else if(lineType == 1){ // coordenada texture
+
+			}else if(lineType == 2){ // normals
+
+			}else if(lineType == 3){ // face
+
+			}
+			
 		}
 	}
 
