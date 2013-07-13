6677359fe1ad97b31830003aa20fa54565e58528
guilhermesft <guilherme.sft@gmail.com>
2013-07-13 01:04:01 -0300

    Tentativa

--- opengl/space/Space.cpp
+++ opengl/space/Space.cpp
@@ -25,56 +25,71 @@
 #include "ObjParserWavefront.h"
 
 void CreateBatch();
-GLBatch* ReadObjFile(const char* path);
-
 // Classes auxiliares do GLTools
 GLShaderManager		shaderManager;     //The shader manager of GLTools
-GLFrame			cameraFrame;       //Frame of reference for the camera
-GLFrustum		viewFrustum;       //View frustum
+GLFrame				cameraFrame;       //Frame of reference for the camera
+GLFrustum			viewFrustum;       //View frustum
 GLMatrixStack		modelViewMatrix;   //Modelview Matrix
 GLMatrixStack		projectionMatrix;  //Projection Matrix
-GLGeometryTransform 	transformPipeline; //Geometry Transform Pipeline
+GLGeometryTransform transformPipeline; //Geometry Transform Pipeline
 
 // Parametros da camera
-M3DVector3f		gCameraPosition =   {0.0f, 0.5f, 5.0f};    //Camera position
-M3DVector3f		gCameraLookAt =     {0.0f, 0.0f, 0.0f};     //Reference point to look at
-M3DVector3f		gCameraUp =         {0.0f, 1.0f, 0.0f};	    //Camera up direction
-GLfloat			gCameraYaw =        0.0f;					//Carmera Yaw
+M3DVector3f			gCameraPosition =   {0.0f, 0.5f, 0.0f};    //Camera position
+M3DVector3f			gCameraLookAt =     {0.0f, 0.0f, 1.0f};     //Reference point to look at
+M3DVector3f			gCameraUp =         {0.0f, 1.0f, 0.0f};	    //Camera up direction
+GLfloat				gCameraYaw =        0.0f;					//Carmera Yaw
 
 
 // Batchs
-GLBatch			axisBatch;			// Batch de geometria dos eixos
-GLBatch			linesBatch;			// Batch de geometria das linhas
+GLBatch				axisBatch;			// Batch de geometria dos eixos
+GLBatch				linesBatch;			// Batch de geometria das linhas
 GLTriangleBatch		sphereBatch;        // Batch de geometria da esfera
-GLBatch*			nave;
+GLBatch             spaceBatch;
+
+// #MOD
+GLBatch             cubeBatch;
 
 // Variaveis auxiliares
-int                 windowWidth = 800;          // Largura da janela
-int                 windowHeight = 600;         // Altura da janela
+int                 windowWidth = 600;          // Largura da janela
+int                 windowHeight = 500;         // Altura da janela
 clock_t             lastClock = clock();        // Ticks do relógio
 float               updateFrequency = 1.0;      // Frequencia de atualização da lógica do game, em segundos
 
+GLuint textureIndex1;
+GLuint textureIndex2;
 
-// #MOD
-GLuint texture1Index;
-GLuint texture2Index;
-
-// #MOD - 3
 GLuint shaderID;
-GLuint uniformTexUnit;
+
+GLuint uniformTextUnit1;
+GLuint uniformTextUnit2;
+
 GLuint uniformMVPMatrix;
 
-// #MOD
-GLuint uniformTexUnit1;
+GLuint uniformMVMatrix;
+GLuint uniformPMatrix;
+GLuint uniformNMatrix;
 
-//***********************************************************************
-GLfloat		*vertices;
-GLuint		vbo_vertex;
+ObjParserWavefront      *parser = new ObjParserWavefront();
+//ObjModel                *model = new ObjModel();
+GLfloat                 *vertices;
+GLfloat                 *textures;
+GLuint                  vertice_count;
 
-ObjFile		*objNave;
+M3DVector4f diffuseColor = {0.7, 0.5, 0.5, 1.0};
+M3DVector4f ambientColor = {0.4, 0.4, 0.4, 1.0};
+M3DVector4f specularColor = {10.0, 4.0, 4.0, 1.0};
 
+M3DVector3f lightPosition = {10, 5, 8};
 
-//***********************************************************************
+GLuint uniformLightPosition;
+GLuint uniformAmbientColor;
+GLuint uniformDiffuseColor;
+GLuint uniformSpecularColor;
+
+GLuint vbo_vertex;
+GLuint vbo_textures;
+
+float rotation = 0.0;
 
 /* KeyboardKeys */
 
@@ -113,50 +128,188 @@ void SpecialKeys(int key, int x, int y) {
 /* GameRender */
 
 void GameRender(void) {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Limpa os buffers e deixa com a cor definida
-	glViewport(0, 0, windowWidth, windowHeight); // Configura a viewport de acordo com o tamanho da tela
+	 glClearColor(0.3, 0.3, 0.3, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glViewport(0, 0, windowWidth, windowHeight);
     
-	// Desenha a cena
-	shaderManager.UseStockShader(GLT_SHADER_SHADED, transformPipeline.GetModelViewProjectionMatrix());
-	linesBatch.Draw();
+    shaderManager.UseStockShader(GLT_SHADER_SHADED, transformPipeline.GetModelViewProjectionMatrix());
+    linesBatch.Draw();
 	axisBatch.Draw();
-//	sphereBatch.Draw();
-//	nave->Draw();   
-	M3DVector4f color = {0.6, 0.1, 0.1, 1.0};
     
-	// ESFERA VERMELHA
-	modelViewMatrix.PushMatrix();
-	modelViewMatrix.Translate(10, 2, 10);
-	shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), color);
-	sphereBatch.Draw();
-	modelViewMatrix.PopMatrix(); 
+    M3DVector4f color = {0.6, 0.1, 0.1, 1.0};
+    
+    // ESFERA VERMELHA
+    modelViewMatrix.PushMatrix();
+    modelViewMatrix.Translate(10, 2, 10);
+    shaderManager.UseStockShader(GLT_SHADER_FLAT, transformPipeline.GetModelViewProjectionMatrix(), color);
+    sphereBatch.Draw();
+    modelViewMatrix.PopMatrix();
+    
+    // ESFERA TEXTURIZADA
+    modelViewMatrix.PushMatrix();
+    
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textureIndex2);
+    
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, textureIndex2);
+    
+    glUseProgram(shaderID);
 
-	glEnableVertexAttribArray(GLT_ATTRIBUTE_VERTEX);
-   	glBindBuffer(GL_VERTEX_ARRAY, vbo_vertex);
+    modelViewMatrix.Translate(-5, 0, 2);
+    modelViewMatrix.Rotate(rotation, 0, 1, 0);
     
-	glDrawArrays(GL_LINE_LOOP, 0, objNave->getCountVertices());
+    glUniformMatrix4fv(uniformMVMatrix, 1, GL_FALSE, transformPipeline.GetModelViewMatrix());
+    glUniformMatrix4fv(uniformPMatrix, 1, GL_FALSE, transformPipeline.GetProjectionMatrix());
     
-	glutSwapBuffers();
+    modelViewMatrix.PushMatrix();
+    modelViewMatrix.LoadIdentity();
+    modelViewMatrix.Rotate(rotation, 0, 1, 0);
+
+    glUniformMatrix3fv(uniformNMatrix, 1, GL_FALSE, transformPipeline.GetNormalMatrix());
+    
+    modelViewMatrix.PopMatrix();
+    
+    rotation += 0.01;
+
+    if(rotation > 360)
+        rotation = 0;
+    
+    glUniform1i(uniformTextUnit1, 0);
+    glUniform1i(uniformTextUnit2, 1);
+    
+    glUniform4fv(uniformAmbientColor, 1, ambientColor);
+    glUniform4fv(uniformDiffuseColor, 1, diffuseColor);
+    glUniform4fv(uniformSpecularColor, 1, specularColor);
+    glUniform3fv(uniformLightPosition, 1, lightPosition);
+    
+    glEnableVertexAttribArray(GLT_ATTRIBUTE_VERTEX);
+    glBindBuffer(GL_VERTEX_ARRAY, vbo_vertex);
+    
+    glEnableVertexAttribArray(GLT_ATTRIBUTE_TEXTURE0);
+    glBindBuffer(GL_VERTEX_ARRAY, vbo_textures);
+
+    glDrawArrays(GL_TRIANGLES, 0, vertice_count*3);
+    
+    glDisableVertexAttribArray(GLT_ATTRIBUTE_VERTEX);
+    glDisableVertexAttribArray(GLT_ATTRIBUTE_TEXTURE0);
+    
+    modelViewMatrix.PopMatrix();
+    
+    glutSwapBuffers();
+
 }
 
 
 
 
 
-# pragma mark GameLogic
-
-GLuint LoadTexture(const char* textureFileName){
-	return 0;
+GLuint LoadTexture(const char* textureFileName, GLenum textureIndexUnit){
+    GLuint texIndex = 0;
+    GLint textureWidth;
+    GLint textureHeight;
+    GLint components;
+    GLenum eFormat;
+    
+    GLbyte *textureData = gltReadTGABits(textureFileName, &textureWidth, &textureHeight, &components, &eFormat);
+    
+    glEnable(GL_TEXTURE_2D);
+    glActiveTexture(textureIndexUnit);
+    
+    glGenTextures(1, &texIndex);
+    glBindTexture(GL_TEXTURE_2D, texIndex);
+    
+    glTexImage2D(GL_TEXTURE_2D, 0, components, textureWidth, textureHeight, 0, eFormat, GL_UNSIGNED_BYTE, textureData);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    glGenerateMipmap(GL_TEXTURE_2D);
+    
+    free(textureData);
+    
+    return texIndex;
 }
 
-
-// #MOD - 1
 GLuint LoadShader(std::string vertexShaderStr, std::string fragmentShaderStr){
-	return 0;
+    
+    GLuint programID;
+    GLuint vertexShader;
+    GLuint fragmentShader;
+    
+    GLint status;
+    char log[1024];
+    
+    // VERTEX SHADER
+    const char *vertexShaderChar = vertexShaderStr.c_str();
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderChar, NULL);
+    glCompileShader(vertexShader);
+    
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+    if(status == GL_FALSE){
+        glGetShaderInfoLog(vertexShader, 1024, NULL, log);
+        fprintf(stderr, "Vertex compile failed:\n%s\n", log);
+        glDeleteShader(vertexShader);
+        return 0;
+    }
+    
+    // FRAGMENT SHADER
+    const char *fragmenShaderChar = fragmentShaderStr.c_str();
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmenShaderChar, NULL);
+    glCompileShader(fragmentShader);
+    
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
+    if(status == GL_FALSE){
+        glGetShaderInfoLog(fragmentShader, 1024, NULL, log);
+        fprintf(stderr, "Fragment compile failed:\n%s\n", log);
+        glDeleteShader(fragmentShader);
+        return 0;
+    }
+    
+    programID = glCreateProgram();
+    
+    glAttachShader(programID, vertexShader);
+    glAttachShader(programID, fragmentShader);
+    
+    glBindAttribLocation(programID, GLT_ATTRIBUTE_VERTEX, "vVertex");
+    glBindAttribLocation(programID, GLT_ATTRIBUTE_TEXTURE0, "vTexCoord0");
+    glBindAttribLocation(programID, GLT_ATTRIBUTE_NORMAL, "vNormal");
+    
+    glLinkProgram(programID);
+    
+    glGetProgramiv(programID, GL_LINK_STATUS, &status);
+    
+    if(status == GL_FALSE){
+        glGetProgramInfoLog(programID, 1024, NULL, log);
+        fprintf(stderr, "Link failed:\n%s\n", log);
+        glDeleteProgram(programID);
+        return 0;
+    }
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    glUseProgram(programID);
+    uniformMVMatrix = glGetUniformLocation(programID, "mvMatrix");
+    uniformPMatrix = glGetUniformLocation(programID, "pMatrix");
+    uniformNMatrix = glGetUniformLocation(programID, "nMatrix");
+
+    uniformTextUnit1 = glGetUniformLocation(programID, "textureUnit0");
+    uniformTextUnit2 = glGetUniformLocation(programID, "textureUnit1");
+    
+    uniformLightPosition = glGetUniformLocation(programID, "lightPosition");
+    uniformAmbientColor = glGetUniformLocation(programID, "ambientColor");
+    uniformDiffuseColor = glGetUniformLocation(programID, "diffuseColor");
+    uniformSpecularColor = glGetUniformLocation(programID, "specularColor");
+    
+    return programID;
 }
 
-
-// #MOD
 std::string ReadFile(const char *fileName){
     FILE *f = fopen(fileName, "rb");
     std::string content;
@@ -244,24 +397,21 @@ void MainCycle(void){
 /* Init */
 
 void Init() {
-	glClearColor(0.3, 0.3, 0.3, 1.0);
 	glEnable(GL_DEPTH_TEST); // Habilita o buffer de teste de profundidade
 	glEnable(GL_CULL_FACE); // Habilita o culling de faces, por questoes de performance, desenhando apenas as faces da frente
     
     // Inicializa o gerenciador de shaders
-	shaderManager.InitializeStockShaders();
-	CreateBatch();
-	std::cout << "passou do createbatch" << std::endl;
-	
+    shaderManager.InitializeStockShaders();
     
-    // #MOD - add textureUnit
-   // texture1Index = LoadTexture("/home/vanz/gitroot/VanzCoder/opengl/curso/data/texture/sun.tga");
-   // texture2Index = LoadTexture("/home/vanz/gitroot/VanzCoder/opengl/curso/data/texture/earth.tga");
+    textureIndex1 = LoadTexture("/home/vanz/gitroot/VanzCoder/opengl/space/data/texture/sun.tga", GL_TEXTURE0);
+    textureIndex2 = LoadTexture("/home/vanz/gitroot/VanzCoder/opengl/space/data/texture/earth.tga", GL_TEXTURE1);
     
-  //  std::string vertexShader = ReadFile("/home/vanz/gitroot/VanzCoder/opengl/curso/vertex.v2.vsh");
-   // std::string fragmentShader = ReadFile("/home/vanz/gitroot/VanzCoder/opengl/cursofragment.v2.fsh");
+    std::string vertexShader = ReadFile("/home/vanz/gitroot/VanzCoder/opengl/space/vertex.v1.vsh");
+    std::string fragmentShader = ReadFile("/home/vanz/gitroot/VanzCoder/opengl/space/fragment.v1.fsh");
     
-  //  shaderID = LoadShader(vertexShader, fragmentShader);
+    shaderID = LoadShader(vertexShader, fragmentShader);
+    
+    CreateBatch();
 }
 
 /* Main */
@@ -331,92 +481,7 @@ void CreateBatch(){
 	linesBatch.End();
 	
 	//carrega nave
-	ObjParserWavefront parser;
-	objNave = new ObjFile();
-  	parser.parse("/home/vanz/gitroot/VanzCoder/opengl/space/data/asteroid.obj", objNave); 
-	vertices = objNave->getVerticeArray();
-    glGenBuffers(1, &vbo_vertex);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo_vertex);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) / sizeof(GLfloat), vertices, GL_STATIC_DRAW);
-    
-	glEnableVertexAttribArray(GLT_ATTRIBUTE_VERTEX);
-    
-//	glVertexAttribPointer(GLT_ATTRIBUTE_VERTEX, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat) * 3, 0);
-	glVertexAttribPointer(GLT_ATTRIBUTE_VERTEX, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-	glDisableVertexAttribArray(GLT_ATTRIBUTE_VERTEX);
+	//ObjParserWavefront parser;
+	//objNave = new ObjFile();
+  	//parser.parse("/home/vanz/gitroot/VanzCoder/opengl/space/data/asteroid.obj", objNave); 
 }
-
-// ObjFile* ReadObjFile(const char* path)
-// {
-
-// 	std::string line;
-// 	std::ifstream myfile(path);
-// 	if (myfile.is_open()){
-// 		ObjFile* obj = new ObjFile();
-// 		while ( myfile.good() ){
-// 			getline (myfile,line);
-// 			int lineType = -1;
-// 			if(line[0] == '#'){
-// 				continue;
-// 			}else if(line[0] == 'v' & line[1] == ' '){
-// 				lineType = 0;
-// 			}else if(line[0] == 'v' & line[1] == 't'){
-// 				lineType = 1;
-// 			}else if(line[0] == 'v' & line[1] == 'n'){
-// 				lineType = 2;
-// 			}else if(line[0] == 'f' & line[1] == ' '){
-// 				lineType = 3;
-// 			}
-// 			line.erase(0,2);
-// 			if(line[0] == ' '){
-// 				line.erase(0,1);
-// 			}
-// 			if(line[line.length()-1] == ' '){
-// 				line.erase(line.length() - 1, 1);
-// 			}
-// 			/*-----------------------------------------------------------
-// 					Split string
-// 			------------------------------------------------------------*/
-// 			std::vector<std::string> strings;
-// 			std::size_t found = 0;
-// 			std::size_t newFound = std::string::npos;
-// 			while((newFound = line.find(' ', found)) != std::string::npos){
-// 				strings.push_back(line.substr(found, newFound));
-// 				line.erase(0,newFound+1);
-// 				found = 0;
-// 				newFound = std::string::npos;
-// 			}
-// 				strings.push_back(line.substr(found, newFound));
-
-// 			----------------------------------------------------------
-// 			if(lineType == 0){ // vertice
-// 				float x = boost::lexical_cast<float>(strings[0]);
-// 				float y = boost::lexical_cast<float>(strings[1]);
-// 				float z = boost::lexical_cast<float>(strings[2]);
-// 		//		vertices.push_back(x);
-// 		//		vertices.push_back(y);
-// 		//		vertices.push_back(z);
-// 				std::cout << "x = " << x << " | y = " << y << " | z = " << z << std::endl;
-// 				//obj->Color4f(1.0f, 0.0f, 0.0f, 1.0f);
-// 				//obj->Vertex3f(x,y,z);
-// 			}else if(lineType == 1){ // coordenada texture
-// 				float v = boost::lexical_cast<float>(strings[0]);
-// 				float t = boost::lexical_cast<float>(strings[1]);
-// 				std::cout << "v = " << v << " | t = " << t << std::endl;
-// 				obj->MultiTexCoord2f(0,v,t);
-// 			}else if(lineType == 2){ // normals
-// 				float x = boost::lexical_cast<float>(strings[0]);
-// 				float y = boost::lexical_cast<float>(strings[1]);
-// 				float z = boost::lexical_cast<float>(strings[2]);
-// 				std::cout << "NORMAL -> x = " << x << " | y = " << y << " | z = " << z << std::endl;
-// 				obj->Normal3f(x,y,z);
-// 			}else if(lineType == 3){ // face
-// 				std::cout << "FACES -> 1 = " << strings[0] << " | 2 = " << strings[1] << " | 3 = " << strings[2] << std::endl;
-// 			}
-			
-// 		}
-// 		return obj;
-		
-// 	}
-// }
