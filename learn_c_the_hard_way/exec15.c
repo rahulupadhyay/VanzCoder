bf3dddf3a72630d499abf5018f72f5bda4a7a388
José Guilherme Vanz <jose.vanz@dbappz.com>
2014-01-07 09:57:44 -0200

    Exercise 15. Part 1
    Initial implementation of exercise 15

---
+++ learn_c_the_hard_way/exec15.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+{
+	//create two arrays we care about
+	int ages[] = {23, 43, 12, 89, 2};
+	char *names[] = {"Alan", "Frank", "Mary", "John", "Lisa" };
+
+	//TODO - terminar...
+}
