2b2fe327708831a8d8146cdba4209ec17af4dbdd
José Guilherme Vanz <guilherme.vanz@gmail.com>
2014-01-07 10:09:40 -0200

    Exercise 15
    Exercise 15, part 2

--- learn_c_the_hard_way/exec15.c
+++ learn_c_the_hard_way/exec15.c
@@ -6,5 +6,33 @@ int main(int argc, char * argv[])
 	int ages[] = {23, 43, 12, 89, 2};
 	char *names[] = {"Alan", "Frank", "Mary", "John", "Lisa" };
 
+	//safely get the size of ages
+	int count = sizeof(ages) / sizeof(int);
+	int i = 0;
+	
+	//first way using indexing
+	for(i =0; i < count; i++){
+		printf("%s has %d years alive.\n", names[i], ages[i]);
+	}
+
+	printf("----\n");
+
+	//setup the pointers to the start of the arrays
+	int *cur_age = ages;
+	char **cur_name = names;
+	
+	//second way using pointers
+	for(i = 0; i < count; i++){
+		printf("%s has %d years old.\n", *(cur_name+1), *(cur_age+1));
+	}
+
+	printf("----\n");
+
+	//third way with pointer in stupid complex way
+	for(cur_name = names, cur_age = ages; (cur_age - ages) < count; cur_name++, cur_age++){
+		printf("%s has %d years so far.\n", *cur_name, *cur_age);
+	}
+
+	return 0;
 	//TODO - terminar...
 }
